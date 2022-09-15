c54611cd1baf10cdb8163fbea346e0c289866dca
DigiDocs <digidocs2022@gmail.com>
2022-09-15 15:15:55 +0530

    add api

--- lib/screens/manage_notification.dart
+++ lib/screens/manage_notification.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:masjit_vendor_app/data/model/notice.dart';
+import 'package:masjit_vendor_app/data/model/noticeCreate.dart';
+import 'package:masjit_vendor_app/screens/home.dart';
+import 'package:masjit_vendor_app/utils/constant.dart';
 import 'package:masjit_vendor_app/widget/edit_notice.dart';
+import 'package:http/http.dart' as http;
 
 class ManageNotification extends StatefulWidget {
   const ManageNotification({Key? key}) : super(key: key);
@@ -9,43 +14,84 @@ class ManageNotification extends StatefulWidget {
 }
 
 class _ManageNotificationState extends State<ManageNotification> {
-  final _notice = [];
+  var _notice = [];
+
+  late Future<GetAllNoticesResponseModel> result;
+
+  var getNoticesData;
+
+  @override
+  void initState() {
+    super.initState();
+
+    result = getAllNotices();
+
+    if (mounted) {
+      setState(() {
+        getNoticesData = getAllNotices();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    _show(int i) {
-      showModalBottomSheet(
-          context: context,
-          isScrollControlled: true,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(20),
-            ),
-          ),
-          builder: (context) {
-            return EditNotice();
-          });
-    }
+    return Scaffold(
+      body: FutureBuilder<GetAllNoticesResponseModel>(
+          future: getNoticesData,
+          builder: (context, snapshot) {
+            return ListView.builder(
+                itemCount: snapshot.data?.data?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.only(top: 15),
+                    child: Container(
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20.0,
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  snapshot.data?.data?[index] != null
+                                      ? "${snapshot.data?.data?[index]}"
+                                      : "",
+                                  style: const TextStyle(
+                                      color: Colors.black, fontSize: 17),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  );
+                });
+          }),
+    );
+  }
 
-    return _notice.isNotEmpty
-        ? ListView(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 8,
-              vertical: 4,
-            ),
-            physics: const BouncingScrollPhysics(),
-            children: [
-              for (int i = 0; i < _notice.length; i++)
-                GestureDetector(
-                  onTap: () {
-                    _show(i);
-                  },
-                  child: Card(child: Text(_notice[i])),
-                ),
-            ],
-          )
-        : const Center(
-            child: Text("add notification"),
-          );
+  Future<GetAllNoticesResponseModel> getAllNotices() async {
+    try {
+      final result = await http.get(
+        Uri.parse("http://masjid.exportica.in/api/masjids/${masjid.id}/notice"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${box.get(kToken)}'
+        },
+      );
+
+      if (result.statusCode == 200) {
+        print(result.body);
+      }
+
+      return getAllNoticesResponseModelFromJson(result.body);
+    } catch (e) {
+      throw e;
+    }
   }
 }
--- lib/widget/edit_notice.dart
+++ lib/widget/edit_notice.dart
@@ -1,10 +1,22 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:masjit_vendor_app/data/model/notice.dart';
+import 'package:masjit_vendor_app/data/model/noticeCreate.dart';
+import 'package:http/http.dart' as http;
+import 'package:masjit_vendor_app/screens/home.dart';
+import 'package:masjit_vendor_app/screens/manage_notification.dart';
+import 'package:masjit_vendor_app/utils/constant.dart';
 
-class EditNotice extends StatelessWidget {
+class EditNotice extends StatefulWidget {
   EditNotice({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<EditNotice> createState() => _EditNoticeState();
+}
+
+class _EditNoticeState extends State<EditNotice> {
   final _nameEditController = TextEditingController();
 
   @override
@@ -39,7 +51,8 @@ class EditNotice extends StatelessWidget {
             ),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).pop();
+                  createNotice();
+                  // Navigator.pop(context);
                 },
                 child: const Text('Save'))
           ],
@@ -47,4 +60,66 @@ class EditNotice extends StatelessWidget {
       ),
     );
   }
+
+  Future<NoticesCreateResponseModel> createNotice() async {
+
+
+    Map<String, String> headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer ${box.get(kToken)}'
+    };
+    final msg = jsonEncode({
+      "masjid_id": masjid.id.toString(),
+      "notices": _nameEditController.text.trim()
+    });
+
+    var response = await http.post(
+      Uri.parse("http://masjid.exportica.in/api/masjids/notice/"),
+      headers: headers,
+      body: msg,
+    );
+
+    if (response.statusCode == 200) {
+
+      if(mounted) {
+        setState(() {
+        getAllNotices();
+      });
+      }
+
+      Navigator.pop(context);
+
+
+      print("Yess.. ${response.body}");
+
+      print("Hiii");
+
+      return noticesCreateResponseModelFromJson(response.body);
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to create album.');
+    }
+  }
+
+  Future<GetAllNoticesResponseModel> getAllNotices() async {
+    try {
+      final result = await http.get(
+        Uri.parse("http://masjid.exportica.in/api/masjids/${masjid.id}/notice"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer ${box.get(kToken)}'
+        },
+      );
+
+      if(result.statusCode == 200){
+        print(result.body);
+      }
+
+      return getAllNoticesResponseModelFromJson(result.body);
+    } catch (e) {
+      throw e;
+    }
+  }
+
 }
